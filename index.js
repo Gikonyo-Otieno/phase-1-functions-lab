f2c25c296e28f87652625bca719ca75eea20af51
Gikonyo-Otieno <gikonyootieno@gmail.com>
2022-11-26 18:48:31 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  const hq = 42
+  return Math.abs(blocks - hq);
+ 
+}
+function distanceFromHqInFeet(blocks) {
+    const hq = 42
+    return Math.abs(blocks - hq)*264;
+   
+ }
+
+ function distanceTravelledInFeet(start, destination){
+    const hq = 42
+    return Math.abs(start - destination)*264;
+ }
+
+ function calculatesFarePrice(start, destination){
+   let travel = distanceTravelledInFeet(start, destination);
+   if (travel <= 400){
+    return 0;
+   }
+    if (travel >= 400 && travel <= 2000) {
+    return (travel - 400) * 0.02;
+   
+   } else if (travel >= 2000 && travel <= 2500) {
+    return 25; 
+   } else if (travel > 2500) {
+    return "cannot travel that far";
+   }
+ }
